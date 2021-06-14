11e03241d1d233d14b41af3a923dd2f10d626d04
hannah hayward <hannahhayward97@gmail.com>
2021-06-14 07:53:07 -0600

    f

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card text-light bg-dark" v-if="!user.isAuthenicated">
+  <div class="card text-light bg-dark">
     <div class="card-header text-center">
       <img :src="account.picture" alt="" class="rounded-pill size">
       <h6 class="mt-3">
@@ -8,13 +8,13 @@
       <div class="card-body">
         <p>{{ account.bio }}</p>
       </div>
-      <div class="card text-light bg-dark" v-if="!user.isAuthenicated">
+      <!-- <div class="card text-light bg-dark" v-if="!user.isAuthenicated">
         <h3>sign in or create an account now!</h3>
         <button class="btn btn-info" @click="login">
           Login
         </button>
-      </div>
-      <div class="card-footer" v-else>
+      </div> -->
+      <div class="card-footer">
         <router-link :to="{ name: 'Account', params:{id: account.id} }" :key="account.id">
           <button class="btn-block btn btn-info">
             Edit your account
